23a4c5d179311934e72ba0725bee4cc565680cd2
David Henri <david.henri.santana@gmail.com>
2025-06-18 19:59:01 -0300

    test(login): add test for validation errors on empty login form

--- cypress/e2e/inputFields.cy.js
+++ cypress/e2e/inputFields.cy.js
@@ -1,5 +1,17 @@
 describe("login page", () => {
-  it("should not let the user ", () => {
-    cy.visit("https://example.cypress.io");
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
+
+  it("should show validation errors when login button is clicked with empty fields", () => {
+    cy.get("[data-js='login-button']").click();
+
+    cy.get("[data-js='error-span-email']")
+      .should("be.visible")
+      .and("contain.text", "Email is required.");
+
+    cy.get("[data-js='error-span-password']")
+      .should("be.visible")
+      .and("contain.text", "Password is required.");
   });
 });
